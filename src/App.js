279698bd6a632dae8e4d1337b5b28580660d8235
JoaoVSouto <joaovitorss99@hotmail.com>
2020-10-27 23:25:19 -0300

    Creates logic for getting and listing repositories from API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
-import "./styles.css";
+import api from './services/api';
+
+import './styles.css';
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const { data } = await api.get('repositories');
+      setRepositories(data);
+    })();
+  }, []);
+
   async function handleAddRepository() {
     // TODO
   }
@@ -14,13 +25,14 @@ function App() {
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
